85646e1297417998a31be24ebe0b9a167d1f740b
anderland <pierpaolo.bozzano@gmail.com>
2021-05-30 19:40:36 -0400

    + next

---
+++ mulino/components/layout.js
@@ -0,0 +1,10 @@
+const Layout = ({ children, seo }) => (
+  <>
+    {/* <Nav categories={categories} /> */}
+    {/* <Navbar /> */}
+    {children}
+    {/* <Footer /> */}
+  </>
+);
+
+export default Layout;
---
+++ mulino/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+const Home = ({}) => {
+  return <p>Hello World</p>;
+};
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+  };
+};
+
+export default Home;
